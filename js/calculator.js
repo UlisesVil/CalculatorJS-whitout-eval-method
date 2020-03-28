718a4e6866289580c3d1a323b4b97da4766fd641
UlisesV <ulisesvil5@hotmail.com>
2020-03-28 06:27:49 -0600

    enter key fixed, visuals finished and Dot working

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,6 @@
 "use strict"
-window.onload =function(){
-    document.onkeypress = keys;
-}
 
 
-function keys(event){
-    
-    console.log(String.fromCharCode(event.charCode));
-    opLine.innerHTML+=String.fromCharCode(event.charCode);
-   
-}
 
 let opLine = document.getElementById("opLine");
 let resLine = document.getElementById("resLine");
@@ -22,59 +13,11 @@ const buttons= document.getElementById("numbers");
     let operationComplete = false;
     const last = () => opLine.innerHTML.substring(opLine.innerHTML.length-1)
 
-opLine.innerHTML=0;
-resLine.innerHTML=0;
-    
-
-function dot(){
-    let dot=".";
-    let obtaindot = opLine.innerHTML;
-    let dotpos= obtaindot.lastIndexOf(dot)
-    console.log(dotpos);
-
-    let plus="+";
-    let obtainplus = opLine.innerHTML;
-    let pluspos= obtainplus.lastIndexOf(plus)
-    console.log(pluspos);
-
-    let min="-";
-    let obtainmin = opLine.innerHTML;
-    let minpos= obtainmin.lastIndexOf(min)
-    console.log(minpos);
-
-    let mult="*";
-    let obtainmult = opLine.innerHTML;
-    let multpos= obtainmult.lastIndexOf(mult)
-    console.log(multpos);
-
-    let div="/";
-    let obtaindiv = opLine.innerHTML;
-    let divpos= obtaindiv.lastIndexOf(div)
-    console.log(divpos);
-
-    let pow="^";
-    let obtainpow = opLine.innerHTML;
-    let powpos= obtainpow.lastIndexOf(pow)
-    console.log(powpos);
+    opLine.innerHTML=0;
+    resLine.innerHTML=0;
 
 
-
-    if(pluspos==-1 && dotpos==-1 || dotpos==-1 && minpos==-1 ||dotpos==-1 && multpos==-1 ||dotpos==-1 && divpos==-1 ||dotpos==-1 && powpos==-1 ){
-        opLine.innerHTML=opLine.innerHTML+dot;
-        console.log(dotpos);
-        console.log(pluspos);
-            }
-    if(pluspos>dotpos || minpos>dotpos || multpos>dotpos || divpos>dotpos || powpos>dotpos){
-        opLine.innerHTML=opLine.innerHTML+dot;
-        console.log(dotpos);
-        console.log(pluspos);
-    }
-    
-
-}
-
-
-
+//Write Numbers
     let typing = text =>{
         if(opLine.innerHTML == 0) opLine.innerHTML = "";
 
@@ -90,11 +33,10 @@ function dot(){
             console.log(operationComplete);
 
         }
-
         if(isNaN(last()) && isNaN(text)){
         opLine.innerHTML=opLine.innerHTML.substring(0,opLine.innerHTML.length-1)
         }
-        
+     
         opLine.innerHTML+=text;
     }
 
@@ -123,60 +65,165 @@ function dot(){
         }
     })
 
-                               
-//Change Sign +/-
-const lastValue = () => opLine.innerHTML.substring(opLine.innerHTML.length-1)
 
-const changeSign=()=>{
-    let lastNumber= "";
-    let position = 0;
+// Keys control
+    let buttons2 = document.getElementsByClassName("buttons");
+    console.log(buttons2);
+    let array_de_strings = [];
 
-    if (!isNaN(lastValue())){
-        for (let i = opLine.innerHTML.length-1; i>0; i--){
-            if(isNaN(opLine.innerHTML[i])){
-                position = i+1;
-                break;
-            }
-        } 
+    for (var i = 0; i < buttons2.length; i++) {
+        console.log(buttons2[i].textContent);
+        array_de_strings.push(buttons2[i].textContent);
     }
-    lastNumber = opLine.innerHTML.substring(position);
-    opLine.innerHTML=opLine.innerHTML.replace(lastNumber, `( ${lastNumber*-1})`)
+
+    let newarray=array_de_strings.join(', ');
+    console.log(newarray);
+    console.log(array_de_strings);
+
+    function playkey(e){
+        let key2=String.fromCharCode(event.keyCode);
+        let busqueda = array_de_strings.find(element=> element == key2);
+        console.log(busqueda);
+        console.log(key2);
+        console.log(event.keyCode);
+
+        if(busqueda !== undefined){
+            switch (busqueda){
+                case "C":  clear(); break;
+                case "+/-":  changeSign(); break;
+                case "BS":  backSpace(); break;
+                case "=":  ""; break;        //it's empty because its function is in operate() and interfere in *1
+                case ".": dot2();
+                default: typing(busqueda); break;
+            }
+        }
+        if(event.keyCode=="66"||String.fromCharCode(event.keyCode)=="b"){
+            
+            backSpace()
+            console.log(event.keyCode);
+        }
+        if( String.fromCharCode(event.keyCode)==buttonEq.innerHTML || event.keyCode=="13"){
+        
+            result.innerHTML = calculate(operate(userInput.innerHTML));
+            operationComplete=true; // *1
+            console.log(event.keyCode)
+        }
+        if( String.fromCharCode(event.keyCode)=="c"){
+            clear()
+            
+        }
+      
+    }
+    window.addEventListener("keypress",playkey);
+    
+
+// Function Dot Key
+    function dot2(){
+        
+        if (opLine.innerHTML.length > 0) {
+            let lastdig = opLine.innerHTML[opLine.innerHTML.length - 1];
+            console.log(lastdig);
+            let array = opLine.innerHTML.split(' ');
+            console.log(array);
+    
+            if ( lastdig == '+' || lastdig == '-' || lastdig == '*'|| lastdig == '/'|| lastdig == '^'|| lastdig == '('|| lastdig == ')') {
+                opLine.innerHTML += '0.'
+            } else if (array[array.length - 1].indexOf('.') == -1) {
+                opLine.innerHTML += '.'
+            }
+        
+        } else {
+            opLine.innerHTML += '0.'
+        }
+    
+    }
+
+
+// Function Dot Button       
+function dot(){
+    let dot = document.querySelector('#buttonDot');
+    dot.addEventListener('click', () => {
+	if (opLine.innerHTML.length > 0) {
+        let lastdig = opLine.innerHTML[opLine.innerHTML.length - 1];
+        console.log(lastdig);
+        let array = opLine.innerHTML.split(' ');
+        console.log(array);
+
+		if ( lastdig == '+' || lastdig == '-' || lastdig == '*'|| lastdig == '/'|| lastdig == '^'|| lastdig == '('|| lastdig == ')') {
+			opLine.innerHTML += '0.'
+		} else if (array[array.length - 1].indexOf('.') == -1) {
+			opLine.innerHTML += '.'
+		}
+	
+	} else {
+        opLine.innerHTML += '0.'
+    }
+})
 }
+
+                      
+//Change Sign +/-
+    const lastValue = () => opLine.innerHTML.substring(opLine.innerHTML.length-1)
+
+    const changeSign=()=>{
+        let lastNumber= "";
+        let position = 0;
+
+        if (!isNaN(lastValue())){
+            for (let i = opLine.innerHTML.length-1; i>0; i--){
+                if(isNaN(opLine.innerHTML[i])){
+                    position = i+1;
+                    break;
+                }
+            } 
+        }
+        lastNumber = opLine.innerHTML.substring(position);
+        opLine.innerHTML=opLine.innerHTML.replace(lastNumber, `( ${lastNumber*-1})`)
+    }
 
 
 //Back Spase
-function backSpace(){
-    let array= opLine.innerHTML.split("");
-    console.log(array);
-    array.pop();
-    console.log(array);
+    function backSpace(){
+        let array= opLine.innerHTML.split("");
+        console.log(array);
+        array.pop();
+        console.log(array);
 
-    let newarray= array.join("");
-    console.log(newarray);
+        let newarray= array.join("");
+        console.log(newarray);
 
-    opLine.innerHTML=newarray;
-    console.log(opLine.innerHTML);
-}
+        opLine.innerHTML=newarray;
+        console.log(opLine.innerHTML);
+    }
 
 
 //Main Function whitout eval() method
-    function operate(s) { 
+    function operate(userV) { 
         // --- Parse a calculation string into an array of numbers and operators 
         var calculation = [], current = ''; 
-        for (var i = 0, ch; ch = s.charAt(i); i++) {
-            if ('()^*/+-'.indexOf(ch) > -1) {
-                if (current == '' && ch == '-') {
+        for (var i = 0, char1; char1 = userV.charAt(i); i++) {
+            if ('()^*/+-'.indexOf(char1) > -1) {
+                if (current == '' && char1 == '-') {
                     current = '-'; 
-                    } else { 
-                        calculation.push(parseFloat(current), ch); current = ''; 
                     } 
-                } else { current += s.charAt(i); } 
-            } if (current != '') { 
-                calculation.push(parseFloat(current)); 
-            } return calculation;
-        } function calculate(calc) { 
+                else { 
+                    calculation.push(parseFloat(current), char1); 
+                    current = ''; 
+                } 
+            } 
+            else { 
+                current += userV.charAt(i); 
+            } 
+        } 
+        if (current != '') { 
+                    calculation.push(parseFloat(current)); 
+        } 
+        return calculation;
+    }
+    function calculate(calc) { 
             // --- Perform a calculation expressed as an array of operators and numbers 
-            var ops = [{'^': (a, b) => Math.pow(a, b)}, 
+            var ops = [
+            {'^': (a, b) => Math.pow(a, b)},
             {'*': (a, b) => a * b, '/': (a, b) => a / b}, 
             {'+': (a, b) => a + b, '-': (a, b) => a - b}], 
             newCalc = [], currentOp; 
@@ -191,7 +238,8 @@ function backSpace(){
                         newCalc.push(calc[j]); 
                     } 
                     console.log(newCalc); 
-                } calc = newCalc; newCalc = []; 
+                } 
+            calc = newCalc; newCalc = []; 
             } 
             if (calc.length > 1) { 
                 console.log('Error: unable to resolve calculation'); 
@@ -199,75 +247,37 @@ function backSpace(){
             } else { 
                 return calc[0]; 
             } 
-        } 
-        var buttonEq = document.getElementById('buttonEq'), 
-            userInput = document.getElementById('opLine'), 
-            result = document.getElementById('resLine');
+    } 
 
-            buttonEq.addEventListener('click', function() { 
-                result.innerHTML = calculate(operate(userInput.innerHTML));
-                operationComplete=true; // *1
-            }); 
+//Equal Button
+    var buttonEq = document.getElementById('buttonEq'), 
+    userInput = document.getElementById('opLine'), 
+    result = document.getElementById('resLine');
 
+    buttonEq.addEventListener('click', function() { 
+        result.innerHTML = calculate(operate(userInput.innerHTML));
+        operationComplete=true; // *1
+        
+    }); 
 
+    
 
-//Evento Keypress
-let input = document.getElementById("button1");    //esta linea no es necesaria ya que esta declarada mas arriba pero se deja con fines ilustrativos
-input.addEventListener("keypress", function(event){
-console.log("[keypress]Tecla presionada", String.fromCharCode(event.keyCode));
-});
-
-
-
-/*
-buttonDot.addEventListener("click",function(){
-let searchDot = ".";
-let indexOfFirst = result.innerHTML.indexOf(searchDot); 
-console.log(indexOfFirst);
-let searchPlus = "+";
-let lastIndexOf = result.innerHTML.lastIndexOf(searchPlus);
-let lastIndexOfDot = result.innerHTML.lastIndexOf(searchDot);    
-console.log(lastIndexOfDot);
-console.log(lastIndexOf);        
-if(indexOfFirst<0){
-            let actual = result.innerHTML;
-            let sumado = buttonDot.innerHTML;
-            result.innerHTML = actual + sumado;
-        }
-        if(lastIndexOf>-1 ){
-            let actual = result.innerHTML;
-            let sumado = buttonDot.innerHTML;
-            result.innerHTML = actual + sumado;
-        }
-        if(lastIndexOfDot=2){
-            let actual = result.innerHTML;
-            let sumado = "";
-            result.innerHTML = actual + sumado;
             
-        }
-        else{
-            let actual = result.innerHTML;
-            let sumado = "";
-            result.innerHTML = actual + sumado;
-        }
-          
-      });
-      */
+// Hide/Show buttons
 
-      /*
-  ///////////////////////////////////        
-        const searchDot = ".";
-    const indexOfFirst = result.innerHTML.indexOf(searchDot); 
-    console.log(indexOfFirst);
+    let hide = document.querySelector("#hide");
+    let keys = document.querySelector("#keys");
+    hide.addEventListener("click", function(){
+    hide.style.display="none";
+    keys.style.display="none";
+    show.style.display="";
+    });
 
-    if(indexOfFirst<0){
-        let actual = result.innerHTML;
-        let sumado = buttonDot.innerHTML;
-        result.innerHTML = actual + sumado;
-    }
-    else{
-        let actual = result.innerHTML;
-        let sumado = "";
-        result.innerHTML = actual + sumado;
-    }*/
+    let show = document.querySelector("#show");
+    show.addEventListener("click", function(){
+    hide.style.display="";
+    keys.style.display="";
+    show.style.display="none";
+    });
 
+    
